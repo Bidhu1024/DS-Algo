013ea98a093997a7f2d701661446baf1598f3c85
Bidhu1024 <bidhu1024av@gmail.com>
2024-12-09 18:16:22 +0530

    Revision for Largest

--- Array/LargestElementinArray.js
+++ Array/LargestElementinArray.js
@@ -52,3 +52,34 @@ function Largest(arr){
     return max
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Revision 2
+
+function Largest(nums){
+    if(nums.length === 0){
+return
+    }
+
+    let max = nums[0];
+    for(let i = 1;i<nums.length;i++){
+        if(nums[i] > max){
+            max = nums[i]
+        }
+    }
+}
